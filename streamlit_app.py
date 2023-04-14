94d6fa91b7653fdbcfb331e163b8b0ba2017c18e
pduddumpudi <74304376+pduddumpudi@users.noreply.github.com>
2023-04-14 06:59:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 # Object notation
 st.sidebar.header("Advance Auto Parts")
 
+
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
